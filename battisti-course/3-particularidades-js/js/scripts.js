b16030c1f13f8f31cfb6ac30962345deb74e8726
JotaP53 <martinsjp53@gmail.com>
2025-06-04 15:59:06 -0300

    update commit

---
+++ battisti-course/3-particularidades-js/js/scripts.js
@@ -0,0 +1,18 @@
+console.log("Primeiro");
+
+console.log("Último");
+
+console.log("Mais um console.log");
+
+var a = 1;
+
+a = 'teste';
+
+console.log(a);
+
+
+// Comentário
+
+/*
+    Comentário 
+*/
---
+++ battisti-course/4-variaveis/js/scripts.js
@@ -0,0 +1,16 @@
+var teste = 1;
+
+// declarando variavel global, não é uma boa prática, melhor declarar o tipo.
+nome = 'Joao';
+
+console.log(teste);
+
+teste = 'JoaoPedro';
+
+console.log(teste);
+
+let testando = 1;
+const ola = 2;
+
+console.log(testando);
+console.log(ola);
