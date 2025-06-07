f822ceba0a8ccb3fb7b14512379bc9375b799b5f
JotaP53 <martinsjp53@gmail.com>
2025-06-07 17:44:42 -0300

    update repository

---
+++ battisti-course/6-string/js/scripts.js
@@ -0,0 +1,18 @@
+var nome = "Joao";
+var sobrenome = "Martins";
+
+var nomeCompleto = nome + " " + sobrenome;
+
+console.log(nome);
+console.log(typeof nome);
+
+console.log(typeof "asd");
+
+console.log(nomeCompleto)
+
+console.log(typeof "5.292929");
+
+var frase = 'Esta é uma frase complexa.';
+
+console.log(frase);
+console.log(typeof frase);
