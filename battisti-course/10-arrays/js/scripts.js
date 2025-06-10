a77b01fe03366532d93f89bd58405493d38d9996
João Pedro Martins Andrade <joao.pandrade@saude.gov>
2025-06-10 14:22:26 -0300

    update repo

---
+++ battisti-course/10-arrays/js/scripts.js
@@ -0,0 +1,22 @@
+var arr = [5, "Joao", true, {teste: 1, teste: 2}];
+
+console.log(arr);
+
+var arr2 = [2,3,4,5,6];
+
+console.log(arr2);
+
+// Asseçando elementos do Array.
+console.log(arr[1]);
+console.log(arr2[0]);
+
+// Inserir elemento no Array
+arr[4] = 10;
+
+console.log(arr);
+
+// Modificando um elemento no Array.
+arr[0] = "Teste";
+
+console.log(arr);
+console.log(typeof arr);
---
+++ battisti-course/11-if/js/scripts.js
@@ -0,0 +1,9 @@
+
+
+console.log("Antes do If");
+
+if(false) {
+    console.log("Testando o If");
+}
+
+console.log("Depois do If");
