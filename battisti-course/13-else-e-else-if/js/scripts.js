4996bbdde40a35c6e432bc4f6d051553906d404f
JotaP53 <martinsjp53@gmail.com>
2025-06-25 14:57:56 -0300

    update repo

---
+++ battisti-course/13-else-e-else-if/js/scripts.js
@@ -0,0 +1,9 @@
+var nome = "Joao";
+
+if (nome == "Pedro") {
+    console.log("O nome dele é Pedro");
+} else if (nome == "João") {
+    console.log("O nome dele é João");
+}else {
+    console.log("Ele possui outro nome.");
+}
